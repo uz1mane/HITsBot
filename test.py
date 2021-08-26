ba3aebf09a542f033184cf8b1abf1e212d4ea0e8
Ivan Uymanov <uimanovivan@gmail.com>
2021-08-26 16:46:23 +0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import discord
+import config
+
+class MyClient(discord.Client):
+	async def on_ready(self):
+		print ('Logged an as {0}!'.format(self.user))
+
+	async def on_message(self, message):
+		print ('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(config.TOKEN)
+
+
+
