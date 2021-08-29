99915b82ae45b9491033782740bc7dd7b0b3bd73
Ivan Uymanov <uimanovivan@gmail.com>
2021-08-29 20:49:16 +0700

    Added code for auto guest role when joining the server. Group roles are not interfering with grade roles.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import discord
 from discord import utils
+from discord.ext import commands
 
 import config
 
@@ -8,6 +9,13 @@ intents = discord.Intents.all()
 class MyClient(discord.Client):
 	async def on_ready(self):
 		print ('Logged an as {0}!'.format(self.user))
+
+	#newly joined user gets guest role
+	# async def on_member_join(member):
+	#     role = discord.utils.get(member.guild.roles, id=config.AUTO_ROLE_ID)
+	#     await member.add_roles(role)
+	#     print("{} joined to channel! Role: {}".format(member, role.id))
+
 
 #for grades
 	async def on_raw_reaction_add(self, payload):
@@ -20,7 +28,7 @@ class MyClient(discord.Client):
 				emoji = str(payload.emoji.name)
 				role = utils.get(message.guild.roles, id=config.ROLES[emoji])
 
-				if (len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER):
+				if (len([i for i in member.roles if i.id not in config.EXCROLES_FOR_GRADES]) <= config.MAX_ROLES_PER_USER):
 					await member.add_roles(role)
 					print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))		
 				else:
@@ -41,7 +49,24 @@ class MyClient(discord.Client):
 				emoji = str(payload.emoji.name)
 				role = utils.get(message.guild.roles, id=config.ROLES[emoji])
 
-				if (len([i for i in member.roles if i.id not in config.EXCROLES]) <= 5):
+				await member.add_roles(role)
+				print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))										
+
+			except KeyError as e:
+				print('[ERROR] Key Error, no role found for ' + emoji)
+			except Exception as e:
+				print(repr(e))
+
+		if (payload.message_id == config.GROUP_POST_ID):
+			channel = self.get_channel(payload.channel_id) 
+			message = await channel.fetch_message(payload.message_id)
+			member = utils.get(message.guild.members, id=payload.user_id)
+
+			try:
+				emoji = str(payload.emoji.name)
+				role = utils.get(message.guild.roles, id=config.ROLES[emoji])
+
+				if (len([i for i in member.roles if i.id not in config.EXCROLES_FOR_GROUPS]) <= config.MAX_ROLES_PER_USER):
 					await member.add_roles(role)
 					print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))		
 				else:
@@ -72,6 +97,23 @@ class MyClient(discord.Client):
 				print(repr(e))
 
 		if (payload.message_id == config.INTERESTS_POST_ID):
+			channel = self.get_channel(payload.channel_id) 
+			message = await channel.fetch_message(payload.message_id)
+			member = utils.get(message.guild.members, id=payload.user_id)
+
+			try: 
+				emoji = str(payload.emoji)
+				role = utils.get(message.guild.roles, id=config.ROLES[emoji])
+			
+				await member.remove_roles(role)
+				print('[SUCCESS] Role {1.name} has been removed from user {0.display_name} '.format(member, role))		
+			
+			except KeyError as e:
+				print('[ERROR] KeyError, no role found for ' + emoji)
+			except Exception as e:
+				print(repr(e))
+
+		if (payload.message_id == config.GROUP_POST_ID):
 			channel = self.get_channel(payload.channel_id) 
 			message = await channel.fetch_message(payload.message_id)
 			member = utils.get(message.guild.members, id=payload.user_id)
